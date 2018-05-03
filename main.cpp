800878545e0be78e389a5d27f5f0353fb1ad087c
bin.yang <bin.yang@alibaba-inc.com>
2018-05-03 09:51:37 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+
+void BubbleSort(int a[] ,int n);
+void QuickSort();
+
+int main() {
+    int a = [11, 4, 5, 12, 3, 7, 8];
+    std::cout<<"hello world"<<std::endl;
+    //BubbleSort();
+}
